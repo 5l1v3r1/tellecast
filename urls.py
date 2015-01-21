ce46154c8469af3cb6e15dcaf5d1f99f3418ae7c
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-01-21 15:53:31 +0530

    urls.py

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from django.conf.urls import include, patterns, url
 from django.contrib import admin, admindocs
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
@@ -13,6 +15,7 @@ admin.site.unregister(Site)
 
 urlpatterns = patterns(
     '',
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include(admindocs.urls)),
 )
