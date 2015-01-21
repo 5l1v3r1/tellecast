2b54d6cdc9de06dd738662dda6448c5ec443899f
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-01-21 18:52:51 +0530

    python-social-auth

--- urls.py
+++ urls.py
@@ -7,17 +7,24 @@ from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
+from social.apps.django_app.default.models import (
+    Association, Nonce, UserSocialAuth,
+)
 
 admin.autodiscover()
 
+admin.site.unregister(Association)
 admin.site.unregister(Group)
+admin.site.unregister(Nonce)
 admin.site.unregister(Site)
+admin.site.unregister(UserSocialAuth)
 
 urlpatterns = patterns(
     '',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include(admindocs.urls)),
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
 )
 
 if settings.DEBUG:
