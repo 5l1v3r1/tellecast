adf8eb48b18d0bb063010394a0c84ab973e8d37e
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-03-26 15:18:42 +0530

    Fixed: User has no status.

--- api/serializers.py
+++ api/serializers.py
@@ -144,7 +144,7 @@ class MasterTell(ModelSerializer):
 class SlaveTell(ModelSerializer):
 
     id = IntegerField(read_only=True)
-    master_tell_id = IntegerField(read_only=True)
+    master_tell_id = IntegerField()
     photo = CharField(required=False)
     first_name = CharField(required=False)
     last_name = CharField(required=False)
