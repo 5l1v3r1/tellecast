60587e400cb5efdd07aa4defd486045e518eb5a2
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-04-22 17:17:12 +0530

    cleanup

--- api/views.py
+++ api/views.py
@@ -1986,9 +1986,9 @@ class Tellcards(DestroyModelMixin, GenericViewSet, ListModelMixin, UpdateModelMi
 
     def get_queryset(self):
         if 'type' in self.request.QUERY_PARAMS:
-            if self.request.QUERY_PARAMS['type'] == 'source':
+            if self.request.QUERY_PARAMS['type'] == 'Source':
                 return models.Tellcard.objects.filter(user_source_id=self.request.user.id).order_by('-timestamp').all()
-            if self.request.QUERY_PARAMS['type'] == 'destination':
+            if self.request.QUERY_PARAMS['type'] == 'Destination':
                 return models.Tellcard.objects.filter(
                     user_destination_id=self.request.user.id,
                 ).order_by(
