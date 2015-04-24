f3ef005420f506ee546b36787b4273da01e0eb51
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-04-24 11:54:33 +0530

    api/middleware.py

--- api/middleware.py
+++ api/middleware.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 class ExceptionMiddleware(object):
 
     def process_exception(self, request, exception):
+        from traceback import print_exc
+        print_exc()
         return JsonResponse(
             data={
                 'error': unicode(exception),
