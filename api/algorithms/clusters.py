fe2525f699aca14ab4685523dc45740f6fb067b2
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-05-01 13:42:52 +0530

    Radar - Groups

--- api/algorithms/clusters.py
+++ api/algorithms/clusters.py
@@ -4,10 +4,26 @@ from math import asin, cos, radians, sin, sqrt
 
 from numpy import array
 from scipy.spatial.distance import pdist, squareform
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN, KMeans
 
 
 def get_clusters(items):
+    groups = {}
+    length = len(items)
+    if not length:
+        return groups.values()
+    co_ordinates = array([[item[1].x, item[1].y] for item in items])
+    distances = squareform(pdist(co_ordinates, (lambda one, two: get_distance(one[0], one[1], two[0], two[1]))))
+    resource = DBSCAN(algorithm='ball_tree', eps=10, min_samples=1)
+    indices = resource.fit_predict(distances).tolist()
+    for key, value in enumerate(indices):
+        if value not in groups:
+            groups[value] = []
+        groups[value].append(items[key])
+    return groups.values()
+
+
+def get_clusters_old(items):
     groups = {}
     length = len(items)
     if not length:
