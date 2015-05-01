1ad6a9c574d24a78907ac6ca014fb6d0bd0537e2
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-05-01 10:32:44 +0530

    Remove `tellzone` from `users` in `GET /api/radar/`

--- api/serializers.py
+++ api/serializers.py
@@ -1400,28 +1400,7 @@ class RadarGetRequest(Serializer):
     radius = FloatField()
 
 
-class RadarGetResponseUsersItemsTellzone(Tellzone):
-
-    class Meta:
-
-        fields = (
-            'id',
-            'name',
-            'photo',
-            'location',
-            'phone',
-            'url',
-            'hours',
-            'point',
-            'inserted_at',
-            'updated_at',
-        )
-        model = models.Tellzone
-
-
 class RadarGetResponseUsersItems(ModelSerializer):
-
-    tellzone = SerializerMethodField(required=False)
 
     class Meta:
 
@@ -1432,17 +1411,8 @@ class RadarGetResponseUsersItems(ModelSerializer):
             'last_name',
             'location',
             'description',
-            'tellzone',
         )
         model = models.User
-
-    def get_tellzone(self, instance):
-        user_location = models.UserLocation.objects.filter(user_id=instance.id).order_by('-timestamp').first()
-        if not user_location:
-            return
-        if not user_location.tellzone:
-            return
-        return RadarGetResponseUsersItemsTellzone(user_location.tellzone).data
 
 
 class RadarGetResponseUsers(Serializer):
