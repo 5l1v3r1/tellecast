9edaab405bed2e89c0dd602d6078b51ac809368c
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-05-26 21:55:27 +0530

    Push Notifications

--- api/celery.py
+++ api/celery.py
@@ -20,7 +20,7 @@ def push_notifications(user_id, json):
     from api import models
     for device in models.DeviceAPNS.objects.filter(user_id=user_id).order_by('id').all():
         try:
-            device.send_message(dumps(json))
+            device.send_message(json)
         except Exception:
             from traceback import print_exc
             print_exc()
