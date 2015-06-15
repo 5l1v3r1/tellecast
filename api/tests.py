1917e0ef9eff337500c4fe8481672610b05004f5
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-06-15 16:01:31 +0530

    api/tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,360 @@
 # -*- coding: utf-8 -*-
+
+from django.contrib.gis.geos import fromstr
+from django.test import TransactionTestCase
+from mixer.backend.django import mixer
+from rest_framework.test import APIClient
+
+from api import models
+
+mixer.register(
+    'api.Tellzone',
+    hours={
+        'Mon': '09:00 AM to 05:00 PM',
+        'Tue': '09:00 AM to 05:00 PM',
+        'Wed': '09:00 AM to 05:00 PM',
+        'Thu': '09:00 AM to 05:00 PM',
+        'Fri': '09:00 AM to 05:00 PM',
+        'Sat': '09:00 AM to 05:00 PM',
+        'Sun': '09:00 AM to 05:00 PM',
+    },
+    point=fromstr('POINT(1.00 1.00)'),
+)
+
+
+class Ads(TransactionTestCase):
+
+    def setUp(self):
+        self.user = mixer.blend('api.User')
+
+        self.count = 10
+        mixer.cycle(self.count).blend('api.Ad')
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user.token))
+
+    def test_a(self):
+        response = self.client.get('/api/ads/', format='json')
+        assert len(response.data) == self.count
+        assert response.status_code == 200
+
+
+class Blocks(TransactionTestCase):
+
+    def setUp(self):
+        self.user_1 = mixer.blend('api.User')
+        self.user_2 = mixer.blend('api.User')
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user_1.token))
+
+    def test_a(self):
+        response = self.client.get('/api/blocks/', format='json')
+        assert response.data == []
+        assert response.status_code == 200
+
+        response = self.client.post(
+            '/api/blocks/',
+            {
+                'user_destination_id': self.user_2.id,
+                'report': True,
+            },
+            format='json',
+        )
+        assert response.data == {}
+        assert response.status_code == 200
+
+        response = self.client.get('/api/blocks/', format='json')
+        assert len(response.data) == 1
+        assert response.data[0]['user']['id'] == self.user_2.id
+        assert response.status_code == 200
+
+        response = self.client.post(
+            '/api/blocks/delete/',
+            {
+                'user_destination_id': self.user_2.id,
+                'report': True,
+            },
+            format='json',
+        )
+        assert response.data == {}
+        assert response.status_code == 200
+
+        response = self.client.get('/api/blocks/', format='json')
+        assert response.data == []
+        assert response.status_code == 200
+
+
+class DevicesAPNS(TransactionTestCase):
+
+    def setUp(self):
+        self.user = mixer.blend('api.User')
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user.token))
+
+    def test_a(self):
+        response = self.client.get('/api/devices/apns/', format='json')
+        assert response.data == []
+        assert response.status_code == 200
+
+        dictionary = {
+            'name': '1',
+            'device_id': '1',
+            'registration_id': '1',
+        }
+
+        response = self.client.post('/api/devices/apns/', dictionary, format='json')
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        id = response.data['id']
+
+        response = self.client.get('/api/devices/apns/', format='json')
+        assert len(response.data) == 1
+        assert response.data[0]['id'] == id
+        assert response.data[0]['name'] == dictionary['name']
+        assert response.data[0]['device_id'] == dictionary['device_id']
+        assert response.data[0]['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 200
+
+        dictionary['name'] = '2'
+        dictionary['registration_id'] = '2'
+
+        response = self.client.post('/api/devices/apns/', dictionary, format='json')
+        assert response.data['id'] == id
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        response = self.client.get('/api/devices/apns/', format='json')
+        assert len(response.data) == 1
+        assert response.data[0]['id'] == id
+        assert response.data[0]['name'] == dictionary['name']
+        assert response.data[0]['device_id'] == dictionary['device_id']
+        assert response.data[0]['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 200
+
+        dictionary = {
+            'name': '2',
+            'device_id': '2',
+            'registration_id': '2',
+        }
+
+        response = self.client.post('/api/devices/apns/', dictionary, format='json')
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        response = self.client.get('/api/devices/apns/', format='json')
+        assert len(response.data) == 2
+        assert response.status_code == 200
+
+        response = self.client.delete('/api/devices/apns/{id}/'.format(id=id), format='json')
+        assert response.data == {}
+        assert response.status_code == 200
+
+        response = self.client.get('/api/devices/apns/', format='json')
+        assert len(response.data) == 1
+        assert response.status_code == 200
+
+
+class DevicesGCM(TransactionTestCase):
+
+    def setUp(self):
+        self.user = mixer.blend('api.User')
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user.token))
+
+    def test_a(self):
+        response = self.client.get('/api/devices/gcm/', format='json')
+        assert response.data == []
+        assert response.status_code == 200
+
+        dictionary = {
+            'name': '1',
+            'device_id': '0x1',
+            'registration_id': '1',
+        }
+
+        response = self.client.post('/api/devices/gcm/', dictionary, format='json')
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        id = response.data['id']
+
+        response = self.client.get('/api/devices/gcm/', format='json')
+        assert len(response.data) == 1
+        assert response.data[0]['id'] == id
+        assert response.data[0]['name'] == dictionary['name']
+        assert response.data[0]['device_id'] == dictionary['device_id']
+        assert response.data[0]['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 200
+
+        dictionary['name'] = '2'
+        dictionary['registration_id'] = '2'
+
+        response = self.client.post('/api/devices/gcm/', dictionary, format='json')
+        assert response.data['id'] == id
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        response = self.client.get('/api/devices/gcm/', format='json')
+        assert len(response.data) == 1
+        assert response.data[0]['id'] == id
+        assert response.data[0]['name'] == dictionary['name']
+        assert response.data[0]['device_id'] == dictionary['device_id']
+        assert response.data[0]['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 200
+
+        dictionary = {
+            'name': '2',
+            'device_id': '0x2',
+            'registration_id': '2',
+        }
+
+        response = self.client.post('/api/devices/gcm/', dictionary, format='json')
+        assert response.data['name'] == dictionary['name']
+        assert response.data['device_id'] == dictionary['device_id']
+        assert response.data['registration_id'] == dictionary['registration_id']
+        assert response.status_code == 201
+
+        response = self.client.get('/api/devices/gcm/', format='json')
+        assert len(response.data) == 2
+        assert response.status_code == 200
+
+        response = self.client.delete('/api/devices/gcm/{id}/'.format(id=id), format='json')
+        assert response.data == {}
+        assert response.status_code == 200
+
+        response = self.client.get('/api/devices/gcm/', format='json')
+        assert len(response.data) == 1
+        assert response.status_code == 200
+
+
+class Home(TransactionTestCase):
+
+    def setUp(self):
+        self.user = mixer.blend('api.User')
+
+        mixer.cycle(5).blend('api.User')
+        mixer.cycle(5).blend('api.Tellzone')
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user.token))
+
+    def test_a(self):
+        dictionary = {
+            'latitude': 1.00,
+            'longitude': 1.00,
+            'dummy': 'Yes',
+        }
+
+        response = self.client.get('/api/home/connections/', dictionary, format='json')
+        assert len(response.data) == 5
+        assert response.status_code == 200
+
+        response = self.client.get('/api/home/statistics/frequent/', dictionary, format='json')
+        assert 'views' in response.data
+        assert 'today' in response.data['views']
+        assert 'total' in response.data['views']
+        assert 'saves' in response.data
+        assert 'today' in response.data['saves']
+        assert 'total' in response.data['saves']
+        assert 'users' in response.data
+        assert 'area' in response.data['users']
+        assert 'near' in response.data['users']
+        assert response.status_code == 200
+
+        response = self.client.get('/api/home/statistics/infrequent/', dictionary, format='json')
+        assert 'views' in response.data
+        assert 'days' in response.data['views']
+        assert 'weeks' in response.data['views']
+        assert 'months' in response.data['views']
+        assert 'saves' in response.data
+        assert 'days' in response.data['saves']
+        assert 'weeks' in response.data['saves']
+        assert 'months' in response.data['saves']
+        assert response.status_code == 200
+
+        response = self.client.get('/api/home/tellzones/', dictionary, format='json')
+        assert len(response.data) == 5
+        assert response.status_code == 200
+
+
+class MasterTells(TransactionTestCase):
+    pass
+
+
+class Messages(TransactionTestCase):
+    pass
+
+
+class Notifications(TransactionTestCase):
+    pass
+
+
+class Radar(TransactionTestCase):
+    pass
+
+
+class Shares(TransactionTestCase):
+    pass
+
+
+class SlaveTells(TransactionTestCase):
+    pass
+
+
+class Tellcards(TransactionTestCase):
+    pass
+
+
+class Tellzones(TransactionTestCase):
+    pass
+
+
+class Users(TransactionTestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_a_deauthenticate(self):
+        self.user = mixer.blend('api.User')
+
+        assert self.user.is_signed_in is False
+
+        self.user.is_signed_in = True
+        self.user.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION=get_header(self.user.token))
+
+        response = self.client.post('/api/deauthenticate/', format='json')
+        assert response.data == {}
+        assert response.status_code == 200
+
+        self.user = models.User.objects.get_queryset().filter(id=self.user.id).first()
+        assert self.user is not None
+        assert self.user.is_signed_in is False
+
+    def test_b(self):
+        pass
+
+    def test_c_tellzones(self):
+        pass
+
+    def test__profile(self):
+        pass
+
+
+def get_header(token):
+        return 'Token {token}'.format(token=token)
