ebe69424c840144d8f1db038d600f0f3d1ece2c9
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-07-28 14:16:16 +0530

    Devices

---
+++ api/migrations/0035_auto_20150728_1416.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0034_userlocation_location'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='deviceapns',
+            name='device_id',
+            field=django_extensions.db.fields.UUIDField(auto=False, max_length=255, verbose_name='Device ID', db_index=True),
+        ),
+    ]
