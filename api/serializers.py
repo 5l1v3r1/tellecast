c4508c218d6f0284f7fe597d115b86171b106845
Mahendra Kalkura <mahendrakalkura@gmail.com>
2015-11-07 17:31:50 +0530

    https://www.pivotaltracker.com/n/projects/1255428/stories/107624602

--- api/serializers.py
+++ api/serializers.py
@@ -988,7 +988,7 @@ class Post(ModelSerializer):
         return models.Post.insert(get_user_id(self.context), self.validated_data)
 
     def update(self):
-        self.instance.update(self.validated_data)
+        self.instance = self.instance.update(self.validated_data)
         self.instance.attachments.get_queryset()._result_cache = None
         self.instance.posts_tellzones.get_queryset()._result_cache = None
         return self.instance
