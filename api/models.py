75ac1cf997b7df3c9017e314f1410e285ace1203
Mahendra Kalkura <mahendra@kalkura.com>
2016-02-26 20:54:50 +0530

    api/models.py

--- api/models.py
+++ api/models.py
@@ -3253,12 +3253,10 @@ def get_master_tells(user_id, tellzone_id, points, radius):
                         api_users_locations.tellzone_id != %s
                     )
                     AND
-                    (
-                        ST_DWithin(
-                            ST_Transform(ST_GeomFromText(%s, 4326), 2163),
-                            ST_Transform(api_users_locations.point, 2163),
-                            %s
-                        )
+                    ST_DWithin(
+                        ST_Transform(ST_GeomFromText(%s, 4326), 2163),
+                        ST_Transform(api_users_locations.point, 2163),
+                        %s
                     )
                     AND
                     api_users_locations.is_casting = TRUE
