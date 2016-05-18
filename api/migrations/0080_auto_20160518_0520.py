1ec94c9f7a6b6c25eec9e0abe63720dc7ab5dc61
Mahendra Kalkura <mahendra@kalkura.com>
2016-05-18 10:50:30 +0530

    Tellzones :: Types and Tellzones :: Statuses

---
+++ api/migrations/0080_auto_20160518_0520.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0079_auto_20160517_0959'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='tellzonestatus',
+            options={'ordering': ('position',), 'verbose_name': 'Tellzone :: Status', 'verbose_name_plural': 'Tellzones :: Statuses'},
+        ),
+        migrations.AlterModelOptions(
+            name='tellzonetype',
+            options={'ordering': ('position',), 'verbose_name': 'Tellzone :: Type', 'verbose_name_plural': 'Tellzones :: Types'},
+        ),
+    ]
