0f2a5a35b794ac2dbcd7365103c5a0155691ac35
Mahendra Kalkura <mahendra@kalkura.com>
2016-07-02 22:59:02 +0530

    https://www.pivotaltracker.com/story/show/124344121

--- api/management/commands/websockets.py
+++ api/management/commands/websockets.py
@@ -574,7 +574,11 @@ class RabbitMQ(object):
                     if 'updated_at' not in message:
                         message['updated_at'] = record['message_updated_at'].isoformat()
                     if 'attachments' not in message:
-                        message['attachments'] = loads(record['message_attachments'])
+                        message['attachments'] = []
+                        try:
+                            message['attachments'] = loads(record['message_attachments'])
+                        except Exception:
+                            pass
                     if 'user_source' not in message:
                         message['user_source'] = {
                             'id': record['message_user_source_id'],
